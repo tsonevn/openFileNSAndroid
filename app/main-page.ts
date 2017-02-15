ce9948d5eb9c877fe62c6f2cef93911d4f89ce15
Nikolay Tsonev <ntsonev@mcsofntsonev.bedford.progress.com>
2017-02-15 16:53:46 +0200

    add ios openFile

--- app/main-page.ts
+++ app/main-page.ts
@@ -11,7 +11,8 @@ import {FilePicker, } from "nativescript-file-picker";
 import {Observable} from "data/observable";
 var application = require("application");
 import {knownFolders} from 'file-system';
-import {openUrl} from "utils/utils"
+import {openUrl, ios} from "utils/utils"
+import {isAndroid} from "platform"
 declare var android:any;
 declare var java:any;
 
@@ -26,38 +27,48 @@ export function navigatingTo(args: EventData) {
 }
 
 export function onTap(){
-    var filepicker =  new FilePicker();
-    filepicker.allowMultiple=false;
-    filepicker.show().then(function(args){
-        console.log(args);
-        var filePath=""+args[0];
-        console.log("------------file path---------------")
-        console.log(filePath);
+    
 
-        // First option 
-        openUrl(filePath);
+        if(isAndroid){
 
+            var filepicker =  new FilePicker();
+            filepicker.allowMultiple=false;
+            filepicker.show().then(function(args){
+                console.log(args);
+                var filePath=""+args[0];
+                console.log("------------file path---------------")
+                console.log(filePath);
+                // First option 
+                openUrl(filePath);
+
+                
+
+                // Secon option 
+
+                //  try
+                // {
+
+                //     var intent = new android.content.Intent(android.content.Intent.ACTION_VIEW);
+                //     intent.setDataAndType(android.net.Uri.parse(filePath.trim()),"application/*");
+                //     intent.addFlags(android.content.Intent.FLAG_ACTIVITY_NEW_TASK);
+                //     application.android.currentContext.startActivity(intent);
+
+                // }
+                // catch (e)
+                // {
+                //     console.log("Error");
+                //     console.log(e);
+                // }
+
+            })
         
-
-        // Secon option 
-
-        //  try
-        // {
-
-        //     var intent = new android.content.Intent(android.content.Intent.ACTION_VIEW);
-        //     intent.setDataAndType(android.net.Uri.parse(filePath.trim()),"application/*");
-        //     intent.addFlags(android.content.Intent.FLAG_ACTIVITY_NEW_TASK);
-        //     application.android.currentContext.startActivity(intent);
-
-        // }
-        // catch (e)
-        // {
-        //     console.log("Error");
-        //     console.log(e);
-        // }
-
-    })
-   
-
+        }
+        else{
+            var documents = knownFolders.currentApp();
+            var filePath = documents.path+"/pdf-sample.pdf";
+            console.log("------------------------file path-----------------------------");
+            console.log(filePath);
+            ios.openFile(filePath);
+        }
 
 }
